dc32cbc530e20b3dae420ffed1a70af3e96e1969
A7eXi <160401973+A7eXi@users.noreply.github.com>
2024-02-18 19:58:49 +0300

    10 строчка

---
+++ Test.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>METANIT.COM</title>
+<meta charset="utf-8" />
+</head>
+<body>
+<h1>
+<?php
+echo "Сохранине";
+?>
+</h1>
+<div>
+<?php
+echo "<h2>Переменные</h2>";
+echo "Типы данных";
+?>
+</div>
+</body>
+</html>
+
